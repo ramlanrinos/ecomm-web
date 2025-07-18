766157e0b9dd11ce754cb506dd949bff574abfea
ramlanrinos <e21336@eng.pdn.ac.lk>
2025-07-18 23:10:58 +0530

    RestController created for testing

---
+++ src/test/java/com/rinos/ecommweb/EcommwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rinos.ecommweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
