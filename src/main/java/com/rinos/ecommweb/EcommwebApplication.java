202e42409d36dd0dedfed98dc86b848b3174ab3d
ramlanrinos <e21336@eng.pdn.ac.lk>
2025-07-18 23:10:23 +0530

    Initial Commit

---
+++ src/main/java/com/rinos/ecommweb/EcommwebApplication.java
@@ -0,0 +1,13 @@
+package com.rinos.ecommweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommwebApplication.class, args);
+    }
+
+}
